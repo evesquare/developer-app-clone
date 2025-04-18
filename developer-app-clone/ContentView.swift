7ac0f941ab0a88d3b380f2653e456a5cbd594162
EveSquare <50450074+EveSquare@users.noreply.github.com>
2025-04-18 16:09:48 +0900

    ContentViewを更新し、画像とイベント情報を表示するリストを追加

--- developer-app-clone/ContentView.swift
+++ developer-app-clone/ContentView.swift
@@ -9,13 +9,33 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            List {
+                VStack {
+                    VStack {
+                        Image("dddepth-187")
+                            .resizable()
+                            .scaledToFill()
+                            .frame(width: .infinity, height: 150)
+                            .clipShape(Rectangle())
+                        VStack(alignment: .leading, spacing: 8) {
+                            Text("JUNE 9-13, 2025")
+                                .font(.headline.weight(.bold))
+                                .opacity(0.7)
+                            Text("A week of technology and creativity")
+                                .font(.title.weight(.bold))
+                            Text("Be there for the reveal of the latest Apple tools, frameworks, and features.")
+                                .opacity(0.7)
+                        }
+                        .padding(15)
+                    }
+                    .background(.gray.gradient.opacity(0.6))
+                }
+                .listRowBackground(Color.clear)
+                .listRowInsets(EdgeInsets())
+            }
+            .navigationTitle("Discover")
         }
-        .padding()
     }
 }
 
